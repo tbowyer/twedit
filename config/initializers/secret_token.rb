b7e97d837b12982f68b28601bbcbd0c16d2dca33
Trevor Bowyer <bowyer@gmail.com>
2017-03-10 23:22:01 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Twedit::Application.config.secret_key_base = 'b45a8a9f51ba4ea1cc98e2b86b87e8a8873b0055507c76bcee0d4ec2d557278cd6da367b8bc162bdf211650f1c7109f38dba5212763ffe225a86fbd230b2966d'
